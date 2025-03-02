8103d8f1b703f5243d4c4b5f919eccfcfdccd45e
Matheus Otenio <matheus.otenio843@gmail.com>
2025-03-02 12:38:32 -0300

    Meu RPS

--- RPS.py
+++ RPS.py
@@ -1,10 +1,33 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
+from collections import Counter
+
+from RPS_game import play
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) < 5:
+        return random.choice(["R", "P", "S"])  # Aleatório no começo para evitar leitura fácil
 
-    return guess
+    # Previsão baseada nos últimos 5 movimentos do oponente
+    last_moves = "".join(opponent_history[-5:])
+    
+    # Dicionário para contar padrões e prever o próximo movimento do oponente
+    patterns = {}
+    for i in range(len(opponent_history) - 5):
+        seq = "".join(opponent_history[i:i+5])
+        next_move = opponent_history[i+5] if i+5 < len(opponent_history) else None
+        if next_move:
+            patterns[seq] = patterns.get(seq, Counter())
+            patterns[seq][next_move] += 1
+
+    # Se já vimos esse padrão antes, prever o próximo movimento do oponente
+    if last_moves in patterns:
+        predicted = patterns[last_moves].most_common(1)[0][0]
+    else:
+        predicted = random.choice(["R", "P", "S"])
+
+    # Jogada que vence o movimento previsto
+    counter = {"R": "P", "P": "S", "S": "R"}
+    return counter[predicted]
